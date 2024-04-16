3d09188be5430dca17a238e5ec2d20b450f1609d
risyafin <risyafin@gmail.com>
2024-04-16 22:45:45 +0700

    add usecase get user

--- core/module/user-usecase.go
+++ core/module/user-usecase.go
@@ -1,2 +1,22 @@
 package module
 
+import (
+	"game_suit/core/entity"
+	"game_suit/core/repository"
+)
+
+type UserUsecase interface {
+	GetUser(id int) (*entity.User, error)
+}
+
+type userUsecase struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserRepository(userRepository repository.UserRepository) UserUsecase {
+	return &userUsecase{userRepository: userRepository}
+}
+
+func (e *userUsecase) GetUser(id int) (*entity.User, error) {
+	return e.userRepository.GetUser(id)
+}
